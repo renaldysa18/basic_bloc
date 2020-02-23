fd14b0fe7e0eeb9d8b758ddb2668323adfc21a52
renaldysab18 <renaldysab18@gmail.com>
2020-02-24 02:52:43 +0700

    [add] adding repository trailer movie

--- lib/resources/repository.dart
+++ lib/resources/repository.dart
@@ -1,8 +1,14 @@
 import 'package:bloc_pattern/models/item_model.dart';
+import 'package:bloc_pattern/models/trailer_model.dart';
 import 'package:bloc_pattern/resources/movie_api_provider.dart';
 
 class Repository {
   final movieApiProvider = MovieApiProvider();
 
+  ///for list movie popular
   Future<ItemModel> fetchMovies() => movieApiProvider.fetchMovieList();
+
+  ///for trailer movie
+  Future<TrailerModel> fetchTrailerMovie(int movieId) => movieApiProvider.fetchTrailerMovie(movieId);
+
 }
